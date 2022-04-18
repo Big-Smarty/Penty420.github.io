7409f9f7dd73edabb99a5127e638aae65c4574c1
Big Smarty <77147643+Big-Smarty@users.noreply.github.com>
2022-04-18 13:17:18 +0200

    Create hmm.php

---
+++ images/hmm.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<head>
+<meta property="og:image" content="https://c.tenor.com/wtv7ADhTUD4AAAAi/dance-happy.gif">
+<meta property="og:type" content="video.other">
+<meta property="og:video:url" content="https://youtu.be/dQw4w9WgXcQ">
+<meta property="og:video:width" content="1280">
+<meta property="og:video:height" content="720">
+</head>
+</html>
